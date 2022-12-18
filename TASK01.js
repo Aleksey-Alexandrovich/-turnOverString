417a2b4ada0ee1331640e62d957c6c9b146b78de
Aleksey <alexeyegorov1987@mail.ru>
2022-12-18 16:45:38 +0300

    add js files

---
+++ TASK01.js
@@ -0,0 +1,11 @@
+'use strict';
+
+for (let v = 0; v < 10; v++) {
+	console.log('-------------');
+	for (let z = 0; z < 10; z++) {
+		console.log(`${v} x ${z} = ${v ** z}`);
+
+	}
+
+}
+
---
+++ TASK03.js
@@ -0,0 +1,5 @@
+const turnOverString = (str) => {
+	return str.split("").reverse().join("");
+}
+
+console.log(turnOverString("Привет мир"));
